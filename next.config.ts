bfb24bd5ec94469a23e7c9c8e5d3e101ca94a141
Muhammed Berisso <muhammedb8826@gmail.com>
2025-09-29 07:41:59 +0300

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone',
+  output: 'export',
   eslint: {
     // Disable ESLint during builds in Docker
     ignoreDuringBuilds: process.env.NODE_ENV === 'production',
